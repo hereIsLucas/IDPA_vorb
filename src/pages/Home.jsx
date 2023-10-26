c2fc52e9757b5087ac254619823bc0d5dbef0c58
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2023-10-26 12:38:26 +0200

    initialization: Project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div className="Home">
+      <h1>Credit Suisse</h1>
+      <h3>The DRAMA, The TEA & The SCANDALES</h3>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
